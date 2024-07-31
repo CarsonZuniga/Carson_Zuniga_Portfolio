1dc9957cba242d8c4a8dc81e71841df6d3cf6e5e
CarsonZuniga <carsonzuniga@gmail.com>
2024-07-30 23:25:48 -0500

    added webpack

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+var webpack = require('webpack');
 
 // https://vitejs.dev/config/
 export default defineConfig({
