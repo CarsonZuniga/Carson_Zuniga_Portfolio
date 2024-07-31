d327557943c054bf9c0a919d9de2091241cc8066
CarsonZuniga <carsonzuniga@gmail.com>
2024-07-30 23:37:06 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-var webpack = require('webpack');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    new webpack.DefinePlugin({
+  plugins: [react(), {
+    define: {
         'import.meta.env.VITE_SPOTIFY_CLIENT_ID': JSON.stringify(import.meta.env.VITE_SPOTIFY_CLIENT_ID),
         'import.meta.env.VITE_SPOTIFY_REDIRECT_URI': JSON.stringify(import.meta.env.VITE_SPOTIFY_REDIRECT_URI),
         'import.meta.env.VITE_YOUTUBE_CLIENT_ID': JSON.stringify(import.meta.env.VITE_YOUTUBE_CLIENT_ID),
         'import.meta.env.VITE_YOUTUBE_REDIRECT_URI': JSON.stringify(import.meta.env.VITE_YOUTUBE_REDIRECT_URI),
-      })
-  ],
+    }
+  }]
 })
